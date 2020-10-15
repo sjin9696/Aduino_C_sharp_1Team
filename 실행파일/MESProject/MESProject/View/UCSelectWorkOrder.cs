16072649c99e5d15bae9e68741b77a3efe27620d
sangheonEN <jteks6@gmail.com>
2020-10-15 11:12:15 +0900

    작업지시조회기능
    작업지시조회기능 1차 commit
    
    이상헌 개인 구현 코드 merge 완료

--- 실행파일/MESProject/MESProject/View/UCSelectWorkOrder.cs
+++ 실행파일/MESProject/MESProject/View/UCSelectWorkOrder.cs
@@ -11,15 +11,52 @@ using MESProject.cmdTable;
 
 namespace MESProject.View
 {
+    // 이상헌
     public partial class UCSelectWorkOrder : UserControl
     {
+        DateTime tstart;
+        DateTime tend;
         MssqlCmdTable mscmd = new MssqlCmdTable();
         public UCSelectWorkOrder()
         {
             InitializeComponent();
+        }
 
-            cbSearch.Text = "종합";
-            dtpFirstDate.Value = DateTime.Now.AddMonths(-1);
+        private void UCSelectWorkOrder_Load(object sender, EventArgs e)
+        {
+            // 기본화면 1달간 조회 상태로 만들기 위해
+            dateTimePicker1.Value = DateTime.Now.AddMonths(-1);
+
+            //dateTimePicker 날짜 표현 수정사항 
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd-dddd";
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.CustomFormat = "yyyy-MM-dd-dddd";
+
+            //ComboBox 칼럼 추가
+            comboBoxSearch.Items.Add("담당자");
+            comboBoxSearch.Items.Add("제품명");
+            comboBoxSearch.Items.Add("LOT 번호");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            tstart = dateTimePicker1.Value;
+            Console.WriteLine(tstart);
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            tend = dateTimePicker2.Value;
+            Console.WriteLine(tend);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string tbt = textBoxSearch.Text;
+            string cbt = comboBoxSearch.Text;
+
+            dataGridView1.DataSource = mscmd.doDataTable(tstart, tend, cbt, tbt);
         }
     }
 }
