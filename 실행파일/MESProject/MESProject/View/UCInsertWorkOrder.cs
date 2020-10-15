9381641e61dbae50d22c2e7573c68d5ea7f72a85
wansu1108 <wan1108@naver.com>
2020-10-15 18:03:25 +0900

    작업지시서 입력,조회 구현
    20201015 18:00

--- 실행파일/MESProject/MESProject/View/UCInsertWorkOrder.cs
+++ 실행파일/MESProject/MESProject/View/UCInsertWorkOrder.cs
@@ -14,6 +14,8 @@ namespace MESProject.View
     public partial class UCInsertWorkOrder : UserControl
     {
         MssqlCmdTable mscmd = new MssqlCmdTable();
+        int i_SelRow;
+        int i_SelCol;
         public UCInsertWorkOrder()
         {
             InitializeComponent();
@@ -24,26 +26,43 @@ namespace MESProject.View
         private void btnPush_Click(object sender, EventArgs e) //김완수
         {
             //그리드뷰에NULL값이 있으면 DB업로드시 NULL값있는행은 업로드가 안됨
-            try
+            //try
+            //{
+            //    foreach (DataGridViewRow item in dgvWorkOrder.Rows)
+            //    {
+            //        mscmd.DBInsert
+            //            (
+            //            dgvWorkOrder.Rows[item.Index].Cells[1].Value.ToString(),//담당자
+            //            DateTime.Parse(dgvWorkOrder.Rows[item.Index].Cells[0].Value.ToString()),//지시일
+            //            DateTime.Parse(dgvWorkOrder.Rows[item.Index].Cells[2].Value.ToString()),//기한일
+            //            dgvWorkOrder.Rows[item.Index].Cells[3].Value as string,//제품명(combobox에 데이터는 이렇게 처리)
+            //            int.Parse(dgvWorkOrder.Rows[item.Index].Cells[4].Value.ToString()),//제품수량
+            //            dgvWorkOrder.Rows[item.Index].Cells[5].Value.ToString(),//회사명
+            //            dgvWorkOrder.Rows[item.Index].Cells[6].Value.ToString()//비고
+            //            );
+            //    }
+            //}
+            //catch (Exception exp)
+            //{
+            //    Console.WriteLine(exp.Message);
+            //}
+
+
+            for(int i=0;i<dgvWorkOrder.Rows.Count-1;i++)
             {
-                foreach (DataGridViewRow item in dgvWorkOrder.Rows)
-                {
-                    mscmd.DBInsert
-                        (
-                        dgvWorkOrder.Rows[item.Index].Cells[1].Value.ToString(),//담당자
-                        DateTime.Parse(dgvWorkOrder.Rows[item.Index].Cells[0].Value.ToString()),//지시일
-                        DateTime.Parse(dgvWorkOrder.Rows[item.Index].Cells[2].Value.ToString()),//기한일
-                        dgvWorkOrder.Rows[item.Index].Cells[3].Value as string,//제품명(combobox에 데이터는 이렇게 처리)
-                        int.Parse(dgvWorkOrder.Rows[item.Index].Cells[4].Value.ToString()),//제품수량
-                        dgvWorkOrder.Rows[item.Index].Cells[5].Value.ToString(),//회사명
-                        dgvWorkOrder.Rows[item.Index].Cells[6].Value.ToString()//비고
-                        );
-                }
+                mscmd.DBInsert
+                    (
+                    dgvWorkOrder.Rows[i].Cells[1].Value.ToString(),//담당자
+                    DateTime.Parse(dgvWorkOrder.Rows[i].Cells[0].Value.ToString()),//지시일
+                    DateTime.Parse(dgvWorkOrder.Rows[i].Cells[2].Value.ToString()),//기한일
+                    dgvWorkOrder.Rows[i].Cells[3].Value as string,//제품명(combobox에 데이터는 이렇게 처리)
+                    int.Parse(dgvWorkOrder.Rows[i].Cells[4].Value.ToString()),//제품수량
+                    dgvWorkOrder.Rows[i].Cells[5].Value.ToString(),//회사명
+                    dgvWorkOrder.Rows[i].Cells[6].Value.ToString()//비고
+                    );
             }
-            catch (Exception exp)
-            {
-                Console.WriteLine(exp.Message);
-            }
+
+
 
         }
 
@@ -52,17 +71,17 @@ namespace MESProject.View
 
             int productQuantity = 0;
             int.TryParse(tbCount.Text, out productQuantity);
-                //////////////////////////이렇게하니까 되네욤////////////////////////
+            //////////////////////////이렇게하니까 되네욤////////////////////////
             string productName = cbProductName.SelectedItem as string;
             ///////////////////////////////////////////////////////////////////
             string employeeName = tbOrnerName.Text;
             string companyName = tbCustomer.Text;
             string remark = tbNull.Text;
-            string startOrder = dateTimePicker1.Value.ToString("yyyy-MM-dd");
-            string endOrder = dateTimePicker2.Value.ToString("yyyy-MM-dd");
+            string startOrder = dateTimePicker1.Value.ToString();
+            string endOrder = dateTimePicker2.Value.ToString();
 
-            
-                
+
+
             if (0 <= productQuantity && productQuantity <= 999)
             {
                 if (productName != null)
@@ -86,9 +105,102 @@ namespace MESProject.View
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            //클리어는 textbox만클리어?
-            //아니면 그리드뷰데이터도 클리어?
-            //그리드뷰 데이터 지우는것도 있으면 좋을거같아요!
+            tbCount.Text = string.Empty;
+            cbProductName.Text = string.Empty;
+            tbOrnerName.Text = string.Empty; ;
+            tbCustomer.Text = string.Empty; ;
+            tbNull.Text = string.Empty;
+            dateTimePicker1.Value = DateTime.Now;
+            dateTimePicker2.Value = DateTime.Now.AddDays(7);
         }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text == "수정")
+            {
+                btnUpdateFalse.Visible = true;
+                btn.Visible = false;
+                dgvWorkOrder.ReadOnly = false;
+            }
+            else if (btn.Text == "수정불가")
+            {
+                btnUpdateTrue.Visible = true;
+                btn.Visible = false;
+                dgvWorkOrder.ReadOnly = true;
+            }
+        }
+
+        private void dgvWorkOrder_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            string s;
+            int a = 0;
+            if (dgvWorkOrder.Columns[e.ColumnIndex].Name.ToString() == "pQ")
+            {
+                s = dgvWorkOrder.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+                if (int.TryParse(s, out a))
+                {
+                    if (a < 0 || 999 < a)
+                    {
+                        dgvWorkOrder.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = 0;
+                        MessageBox.Show("0~999사이의값을 입력해주세요.");
+                    }
+                }
+                else
+                {
+                    dgvWorkOrder.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = 0;
+                    MessageBox.Show("수량을 다시 입력하세요");
+                }
+
+            }
+        }
+
+        private void dgvWorkOrder_MouseUp(object sender, MouseEventArgs e)
+        {
+            //동적으로 좌표를 정할경우
+            //소스로해야함
+            if (e.Button == MouseButtons.Right)
+            {
+                DataGridView.HitTestInfo info = dgvWorkOrder.HitTest(e.X, e.Y);
+                if (info.Type == DataGridViewHitTestType.Cell)
+                {
+                    ContextMenuStrip mnu = new ContextMenuStrip();
+                    //ToolStripMenuItem mnuUpdate = new ToolStripMenuItem("수정");
+                    ToolStripMenuItem mnuCut = new ToolStripMenuItem("삭제");
+
+
+                    //Assign event handlers
+                    //mnuUpdate.Click += new EventHandler(mnuUpdate_Click);
+                    mnuCut.Click += new EventHandler(mnuCut_Click);
+
+                    //Add to main context menu
+                    mnu.Items.AddRange(new ToolStripItem[] { /*mnuUpdate,*/ mnuCut });
+
+                    //Assign to datagridview
+                    dgvWorkOrder.ContextMenuStrip = mnu;
+
+                    i_SelRow = info.RowIndex;
+                    i_SelCol = info.ColumnIndex;
+                }
+
+            }
+
+        }
+        //private void mnuUpdate_Click(object sender, EventArgs e)
+        //{
+        //    foreach (DataGridViewRow row in dgvWorkOrder.Rows)
+        //    {
+        //        row.ReadOnly = true;
+        //    }
+        //    dgvWorkOrder.Rows[i_SelRow].ReadOnly = false;
+        //    dgvWorkOrder.BeginEdit(true);
+        //}
+        private void mnuCut_Click(object sender, EventArgs e)
+        {
+            DataGridView dv = dgvWorkOrder;
+            dgvWorkOrder.Rows.Remove(dgvWorkOrder.Rows[i_SelRow]);
+        }
+
+
     }
 }
